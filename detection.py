78b9a75df0f482e4b368a92fb0cbf2c9bff55bb0
coil398 <konayuta.sou@gmail.com>
2017-02-14 15:22:10 +0900

    modified detection.py

--- detection.py
+++ detection.py
@@ -10,41 +10,43 @@ DETECTED_MALE = 0
 DETECTED_FEMALE = 0
 
 
-async def detect_face(folder, f):
+async def detect_face(folder, targetFolder, f):
     global DETECTED_MALE
     global DETECTED_FEMALE
     print('detecting: ' + f)
-    img = cv2.imread(f)
+    img = cv2.imread(folder + f)
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     dets = detector(img_rgb)
 
     if len(dets) > 0:
-        print('get the face in: ' + f)
-        if folder == './male/':
-            DETECTED_MALE += 1
-        else:
-            DETECTED_FEMALE += 1
+        for d in dets:
+            print('get the face in: ' + f)
+            img = img[d.top() - 30: d.bottom() + 30,
+                      d.left() - 30: d.right() + 30]
+            cv2.imwrite(targetFolder + f, img)
+            if folder == './male/':
+                DETECTED_MALE += 1
+            else:
+                DETECTED_FEMALE += 1
     else:
         print('can\'t detect the face in: ' + f)
 
 
 async def detect_faces(folder):
     print('folder: ' + str(folder))
-    for f in folder[1]:
+    for f in folder[2]:
         print('loading: ' + f)
-        f = folder[0] + f
-        await detect_face(folder[0], f)
+        await detect_face(folder[0], folder[1], f)
 
 
 def main():
-    male = ['./male/']
-    female = ['./female/']
+    male = ['./male/', './detectedMale/']
+    female = ['./female/', './detectedFemale/']
     male.append(os.listdir(male[0]))
     female.append(os.listdir(female[0]))
     loop = asyncio.get_event_loop()
-    asyncio.ensure_future(detect_faces(male))
-    asyncio.ensure_future(detect_faces(female))
-    loop.run_forever()
+    loop.run_until_complete(detect_faces(male))
+    loop.run_until_complete(detect_faces(female))
 
 
 if __name__ == '__main__':
